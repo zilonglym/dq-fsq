9ad6108d80e4aa173a94c154940e52bba10a14b0
zilonglym <992100125@qq.com>
2017-05-14 11:12:04 +0800

    init project

---
+++ src/main/java/com/zilong/fsq/App.java
@@ -0,0 +1,13 @@
+package com.zilong.fsq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
